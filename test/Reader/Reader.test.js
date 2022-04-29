e7707efc0b3a44c1cdaf2c6995929d6a301d8abb
MazinoXX <o.tru08@gmail.com>
2022-04-28 22:08:51 -0500

    Usando linter fix en el proyecto, genial!!

--- test/Reader/Reader.test.js
+++ test/Reader/Reader.test.js
@@ -1,15 +1,15 @@
-const Reader = require('./../../lib/utils/Reader')
+const Reader = require("./../../lib/utils/Reader");
 const fs = require("fs");
 
-describe('Prueba del reader para los explorers', () => {
-    test('Reading package for explorers', () => {
-        const explorer = Reader.readJsonFile("explorers.json")
+describe("Prueba del reader para los explorers", () => {
+    test("Reading package for explorers", () => {
+        const explorer = Reader.readJsonFile("explorers.json");
        
 
         const rawdata = fs.readFileSync("explorers.json");
         const explorers = JSON.parse(rawdata);
 
-        expect(explorer).toEqual(explorers)
-    })
-})
+        expect(explorer).toEqual(explorers);
+    });
+});
 
--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,32 +1,32 @@
-const FizzbuzzService = require('./../../lib/services/FizzbuzzService')
+const FizzbuzzService = require("./../../lib/services/FizzbuzzService");
 
-describe('Preuba del servicio de Fizzbuzz para los explorers', () => {
-    test('1. score = 1', () => {
+describe("Preuba del servicio de Fizzbuzz para los explorers", () => {
+    test("1. score = 1", () => {
 
-        const explorer1 = {name: "Explorer1", score: 1}
-        const explorer = FizzbuzzService.applyValidationInExplorer(explorer1)
+        const explorer1 = {name: "Explorer1", score: 1};
+        const explorer = FizzbuzzService.applyValidationInExplorer(explorer1);
 
-        expect(explorer.score).toBe(1)
-    })
-    test('2. score = 3', () => {
+        expect(explorer.score).toBe(1);
+    });
+    test("2. score = 3", () => {
 
-        const explorer3 = {name: "Explorer3", score: 3}
-        const explorer = FizzbuzzService.applyValidationInExplorer(explorer3)
+        const explorer3 = {name: "Explorer3", score: 3};
+        const explorer = FizzbuzzService.applyValidationInExplorer(explorer3);
 
-        expect(explorer.trick).toBe("FIZZ")
-    })
-    test('3. score = 5', () => {
+        expect(explorer.trick).toBe("FIZZ");
+    });
+    test("3. score = 5", () => {
 
-        const explorer5 = {name: "Explorer5", score: 5}
-        const explorer = FizzbuzzService.applyValidationInExplorer(explorer5)
+        const explorer5 = {name: "Explorer5", score: 5};
+        const explorer = FizzbuzzService.applyValidationInExplorer(explorer5);
 
-        expect(explorer.trick).toBe("BUZZ")
-    })
-    test('4. score = 15', () => {
+        expect(explorer.trick).toBe("BUZZ");
+    });
+    test("4. score = 15", () => {
 
-        const explorer15 = {name: "Explorer15", score: 15}
-        const explorer = FizzbuzzService.applyValidationInExplorer(explorer15)
+        const explorer15 = {name: "Explorer15", score: 15};
+        const explorer = FizzbuzzService.applyValidationInExplorer(explorer15);
 
-        expect(explorer.trick).toBe("FIZZBUZZ")
-    })
-})
+        expect(explorer.trick).toBe("FIZZBUZZ");
+    });
+});
