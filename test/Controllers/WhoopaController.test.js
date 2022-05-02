a216525ce77e15c0ee18ab058302c8abab7cc236
MazinoXX <o.tru08@gmail.com>
2022-05-02 09:07:31 -0500

    Implementando el nuevo metodo en el controlador y su prueba

--- test/Controllers/WhoopaController.test.js
+++ test/Controllers/WhoopaController.test.js
@@ -1,4 +1,4 @@
-const WhoopaController = require('./../../lib/controllers/WhoopaController')
+const WhoopaController = require("./../../lib/controllers/WhoopaController");
 
 describe("Prueba del controlador Whoopa", () => {
     test("1. Metodo get explorerusername", () => {
@@ -8,27 +8,27 @@ describe("Prueba del controlador Whoopa", () => {
             githubUsername: "ajolonauta1",
             score: 1,
             mission: "node"
-          }
+        };
         
 
-        const explorer = WhoopaController.getExplorersUsernameByMission(explorer1.mission)
-        expect(explorer).toEqual(["ajolonauta1"])
-    })
+        const explorer = WhoopaController.getExplorersUsernameByMission(explorer1.mission);
+        expect(explorer).toEqual(["ajolonauta1"]);
+    });
     test("2. Metodo cantidad de explorers en Node", () => {
 
-        const explorer = WhoopaController.getAmountOfExplorersByMission("node")
-        expect(explorer).toBe(5)
-    })
-    test('3. Fizzbuzz para los explorers', () => {
+        const explorer = WhoopaController.getAmountOfExplorersByMission("node");
+        expect(explorer).toBe(5);
+    });
+    test("3. Fizzbuzz with number", () => {
 
         const explorer1 =  {
             name: "Woopa1",
             githubUsername: "ajolonauta1",
             score: 1,
             mission: "node"
-          }
+        };
 
-        const explorer = WhoopaController.fizzBuzzExplorer(explorer1)
-        expect(explorer.score).toBe(1)
-    })
-})
+        const explorer = WhoopaController.fizzBuzzNumber(explorer1.score);
+        expect(explorer).toBe(1);
+    });
+});
