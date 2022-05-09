0a44c8e0b82b0d1bfd2188951bb0198d98a0ddd3
MazinoXX <o.tru08@gmail.com>
2022-05-09 15:10:33 -0500

    Corrigiendo pruebas el controller

--- test/Controllers/WhoopaController.test.js
+++ test/Controllers/WhoopaController.test.js
@@ -12,12 +12,12 @@ describe("Prueba del controlador Whoopa", () => {
         
 
         const explorer = WhoopaController.getExplorersUsernameByMission(explorer1.mission);
-        expect(explorer).toEqual(["ajolonauta1"]);
+        expect(explorer).toEqual(["ajolonauta1","ajolonauta2","ajolonauta3","ajolonauta4",]);
     });
     test("2. Metodo cantidad de explorers en Node", () => {
 
         const explorer = WhoopaController.getAmountOfExplorersByMission("node");
-        expect(explorer).toBe(5);
+        expect(explorer).toBe(4);
     });
     test("3. Fizzbuzz with number", () => {
 
